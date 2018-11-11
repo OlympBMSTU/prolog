40bfa4115284d12d029124e22ce05371a10404cc
Himjune <himjune@mail.ru>
2018-11-11 12:40:00 +0300

    add site and new ver prolog

---
+++ olymps/precache-manifest.137ae1749414bffe27adc69402c49975.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9296a6c6f8c298dae073",
+    "url": "/static/js/main.9296a6c6.chunk.js"
+  },
+  {
+    "revision": "97bb5c65713b41ff404c",
+    "url": "/static/js/1.97bb5c65.chunk.js"
+  },
+  {
+    "revision": "9296a6c6f8c298dae073",
+    "url": "/static/css/main.85ff30e3.chunk.css"
+  },
+  {
+    "revision": "5ce676a0f259ddfe49dffa753d21a027",
+    "url": "/index.html"
+  }
+];
